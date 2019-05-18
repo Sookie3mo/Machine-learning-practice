df2e82de9451eda5f9e4b71f4ee3c6be94b6b3ba
sookie233 <liuyimin@usc.edu>
2019-05-18 15:27:16 -0700

    kmeans

---
+++ Kmeans&GMM/Kmeans.py
@@ -0,0 +1,18 @@
+from math import log
+import operator
+import sys
+import string
+
+
+
+num = 150
+f = open("clusters.txt", 'r+')
+line = f.readline()
+print line
+for i in range(1:num):
+    row [i] = line
+
+
+
+
+
